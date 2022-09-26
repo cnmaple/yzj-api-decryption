c62b380f4effdcb0960cf4738d4d7e5d965522e3
maple <ww1516123@126.com>
2022-09-26 18:03:44 +0800

    delete aes package

--- decryption.go
+++ decryption.go
@@ -3,10 +3,11 @@ package yzj_api_decryption
 import (
 	"bytes"
 	"context"
+	"crypto/aes"
+	"crypto/cipher"
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
-	"github.com/cnmaple/yzj_api_decryption/aes"
 	"io/ioutil"
 	"net/http"
 )
@@ -74,10 +75,63 @@ func DecodeBody(body string, key string) (string, error) {
 	if err != nil {
 		return "", err
 	}
-	jsonStr, err := aes.AesDecrypt(descBytes, []byte(key))
+	jsonStr, err := aesDecrypt(descBytes, []byte(key))
 	if err != nil {
 		fmt.Println(err.Error())
 		return "", err
 	}
 	return string(jsonStr), nil
+}
+
+// pKCS5UnPadding PKCS5.
+func pKCS5UnPadding(origData []byte) []byte {
+	length := len(origData)
+	unpadding := int(origData[length-1])
+	return origData[:(length - unpadding)]
+}
+
+// aesDecrypt ECB PKCS5.
+func aesDecrypt(crypted, key []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	blockMode := NewECBDecrypter(block)
+	origData := make([]byte, len(crypted))
+	blockMode.CryptBlocks(origData, crypted)
+	origData = pKCS5UnPadding(origData)
+	return origData, nil
+}
+
+type ecb struct {
+	b         cipher.Block
+	blockSize int
+}
+
+func newECB(b cipher.Block) *ecb {
+	return &ecb{
+		b:         b,
+		blockSize: b.BlockSize(),
+	}
+}
+
+type ecbDecrypter ecb
+
+// NewECBDecrypter returns a BlockMode which decrypts in electronic code book mode, using the given Block.
+func NewECBDecrypter(b cipher.Block) cipher.BlockMode {
+	return (*ecbDecrypter)(newECB(b))
+}
+func (x *ecbDecrypter) BlockSize() int { return x.blockSize }
+func (x *ecbDecrypter) CryptBlocks(dst, src []byte) {
+	if len(src)%x.blockSize != 0 {
+		panic("crypto/cipher: input not full blocks")
+	}
+	if len(dst) < len(src) {
+		panic("crypto/cipher: output smaller than input")
+	}
+	for len(src) > 0 {
+		x.b.Decrypt(dst, src[:x.blockSize])
+		src = src[x.blockSize:]
+		dst = dst[x.blockSize:]
+	}
 }
