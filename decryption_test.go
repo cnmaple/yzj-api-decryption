29a8ad5334aac39e34a11ec7c2bdb65990f52c63
maple <ww1516123@126.com>
2022-09-28 08:52:13 +0800

    modify test

--- decryption_test.go
+++ decryption_test.go
@@ -1,12 +1,12 @@
 package yzjapidecryption_test
 
 import (
-	"bytes"
 	"context"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	yzjdecryption "github.com/cnmaple/yzjapidecryption"
@@ -36,7 +36,7 @@ func TestYzjDecryptionPlugin(t *testing.T) {
 
 	recorder := httptest.NewRecorder()
 	bodyStr := "xUCqrAxA/cxr0oXA7AyrCmRUs1890qjZgjDiAak/a5n2Lhq1bDLipgEpiKME66uwUHoyDzadAwo9jgJeG/D0IfmYytnyig/LFYNKPaF+szhfAQnQzJufKT3Q8kRpbu9vaHra+mmzcjtyIfwkMPpZEelzSAGDtbkEfv131ZTYgXhQEKqRwP9YYGYxObecPp7uGBNPabpTPCSuFR4AAKaXotZoMFTcD4jQzrsM1C0xSfxdbBEc+jkXOaIHtfdlvmLAHAElq2PzXRFZIlBD1gZf3wV7Uhe3egxeG5MvP4C/A57c2oN6AfACakFXun2sLN+v8wsK2OOt71EJPn29VGGk67322IkvJALrRQLjFreEDuugTwJFSIwmxNHVLzoTp6/ZCveseCO/5k6xW8sOftTxdu2Y/ev3SLgaDxpNim/z94Du3uARpCd6bYAlZL7TKIOm870J0iFL3bx9Zj2XWdiRIttGakhxjOf7oR6cNwPNfahdX4v/J6Pb+RVYOt2q+lq7DDr6t8cSPNWdLe9zTGy+CunnKG8hbPDwXyhS++senoR7NVYAvT/EmsMtnES6ZL9rZv0VsHCmMF+JYbq1fLOr39NSDta20OB5ZCmOxJHHuquacVEzmawC8yAU0cYhZ9H+pACgnIXn7AhY/ODB7IRkOW2Uu7DKPCpajMEH2MMFdVO+Ub2O6Qm49zRN7RsgbXIt6VHsGej/PADER9e+bEs8/JaHA1aRI09tlX3rJrYOzPi7tmRnn3y8Pc93grw0QMaeDzfyC/kMB3AYB1lYArCoIlwa/4Scw043UQcZLP/5298="
-	req, err := http.NewRequestWithContext(ctx, http.MethodPost, "http://localhost", ioutil.NopCloser(bytes.NewBuffer([]byte(bodyStr))))
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, "http://localhost", ioutil.NopCloser(strings.NewReader(bodyStr)))
 	if err != nil {
 		t.Fatal(err)
 	}
